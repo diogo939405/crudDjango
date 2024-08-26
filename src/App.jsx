a6cfe4ff184db6c71fdd546140644fc14457b175
Diogo <Diogo.Favilla@gmail.com>
2024-08-26 18:37:16 -0300

    falta deploy e pequenos ajustes

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,8 @@ function App() {
   const [bookTitle, setBookTitle] = useState()
   const [lancamento, setLancamento] = useState()
   const [newData, setNewData] = useState("")
+  const [edit, setEdit] = useState(false)
+  const [editBookId, setEditBookId] = useState(null);
 
   useEffect(() => {
     fecthBooks()
@@ -43,9 +45,19 @@ function App() {
     } catch (error) {
       console.log(error)
     }
+    setBookTitle('')
+    setLancamento('')
+  }
+
+  const showEdit = (pk, tituloAtual) => {
+    setEdit(true)
+    setEditBookId(pk)
+    setNewData(tituloAtual)
   }
 
   const updateData = async (pk, lancamento) => {
+    setEdit(true)
+    setEditBookId(pk)
     const bookData = {
       bookTitle: newData,
       Lancamento: lancamento
@@ -67,12 +79,14 @@ function App() {
           return book
         }
       }))
+      setEdit(false)
     } catch (error) {
       console.log(error)
     }
   }
 
   const deleteBook = async (pk) => {
+    window.alert('deseja apagar a linha?')
     try {
       const response = await fetch(`http://127.0.0.1:8000/api/books/${pk}`, {
         method: 'DELETE',
@@ -87,26 +101,69 @@ function App() {
   }
 
   return (
-    <>
-      <h1>livros</h1>
-      <div>
-        <input type='text' placeholder='Titulo do livro...' onChange={(e) => setBookTitle(e.target.value)}></input>
-        <input type='number' placeholder='Ano de Lançamento' onChange={(e) => setLancamento(e.target.value)}></input>
-        <button onClick={addBook}>adicionar livro</button>
-        {books.map((book) => (
-          <>
-            <div>
-              <p>Titulo:{book.bookTitle}</p>
-              <p>Lançamento:{book.Lancamento}</p>
-
-              <input type='text' placeholder='alterar titulo...' onChange={(e) => setNewData(e.target.value)} />
-              <button onClick={() => updateData(book.id, book.Lancamento)}>Alterar Titulo</button>
-              <button onClick={() => deleteBook(book.id)}>Apagar</button>
-            </div>
-          </>
-        ))}
+    <div>
+      <div className='titulo'>
+        <p>livros</p>
       </div>
-    </>
+
+      <div className='adicionar'>
+        <span>Adicione novos livros</span>
+        <input type='text' placeholder='Titulo do livro...' onChange={(e) => setBookTitle(e.target.value)}></input>
+        <input type='year' min="1900" max="2024" placeholder='Ano de Lançamento' onChange={(e) => setLancamento(e.target.value)}></input>
+        <button className='styled-button' onClick={addBook}>adicionar livro</button>
+      </div>
+
+      <div className="table-container">
+        <table className="responsive-table">
+
+          <thead>
+            <tr>
+              <th>Nome do livro</th>
+              <th>Ano de lançamento</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {books.map((book) => (
+              <>
+                <tr key={book.id}>
+                  <td key={book.id}>{
+                    edit && editBookId == book.id ? (
+
+                      <input onChange={(e) => setNewData(e.target.value)} value={newData}></input>
+
+                    ) : (
+                      book.bookTitle
+                    )
+                  }
+                  </td>
+                  <td>{book.Lancamento}</td>
+                  <td>
+                    <button className="delete-btn" onClick={() => deleteBook(book.id)}>Apagar</button>
+                    {
+                      !edit ? (
+                        <>
+                          <button className="edit-btn" onClick={() => showEdit(book.id, book.bookTitle)}>Editar</button>
+                        </>
+                      ) : (
+                        <>
+                          <button className="edit-btn" onClick={() => updateData(book.id, book.Lancamento)}>Confirma alteração</button>
+                        </>
+                      )
+                    }
+
+                  </td>
+                </tr>
+              </>
+            ))}
+          </tbody>
+
+        </table>
+      </div>
+
+
+
+    </div>
   )
 }
 
